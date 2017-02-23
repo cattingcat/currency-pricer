15b24ea4eb214f41f4f722a1868785eaf528ce46
mark <mark.martynov@yahoo.com>
2017-02-23 13:38:47 +0300

    Another date fix

--- src/main/kotlin/org/currency_pricer/Utils.kt
+++ src/main/kotlin/org/currency_pricer/Utils.kt
@@ -2,9 +2,6 @@ package org.currency_pricer
 
 import java.text.SimpleDateFormat
 
-/**
- * Created by PC2User on 19.02.2017.
- */
 object Utils {
-    val dateFormat = SimpleDateFormat("dd.mm.yy hh:mm")
+    val dateFormat = SimpleDateFormat("dd.MM.yy hh:mm")
 }
