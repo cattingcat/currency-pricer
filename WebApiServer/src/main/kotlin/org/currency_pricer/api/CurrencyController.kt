a066579ef5d0b3a0c94e8e69b433964b07b739d8
mark <mark.martynov@yahoo.com>
2017-02-23 21:28:55 +0300

    android studio fixes

--- WebApiServer/src/main/kotlin/org/currency_pricer/api/CurrencyController.kt
+++ WebApiServer/src/main/kotlin/org/currency_pricer/api/CurrencyController.kt
@@ -2,11 +2,6 @@ package org.currency_pricer.api
 
 import org.currency_pricer.core.BrokerSpread
 import org.currency_pricer.core.ISnapshotStorage
-import org.currency_pricer.core.ISpreadDataSource
-import org.currency_pricer.sources.BankiruDataSource
-import org.currency_pricer.sources.BrokerDataFactory
-import org.currency_pricer.sources.CompositeDataSource
-import org.currency_pricer.sources.KurscomruDataSource
 import org.currency_pricer.storages.StorageFactory
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
